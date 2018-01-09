22bf6afa17dcdf517bd0dd3ec4936ca0b75f8f98
Robert Laws <roblaws@me.com>
2018-01-09 13:51:32 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,20 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: ["Fiction","Non-Fiction"]}
+  validates :clickbait, on: :title 
+
+  def clickbait
+    case title
+    when "Won't Believe"
+      true
+    when "Secret"
+      true
+    when "Top [number]"
+      true
+    when "Guess"
+      true
+    else
+      false
+    end
+  end  
 end
